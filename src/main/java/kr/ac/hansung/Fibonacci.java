ac736187d6dc1c81c43c195f111c18c8a0eb0c89
Yuseong Na <us9567@naver.com>
2023-11-05 11:39:39 +0900

    modify fibnacci function

--- src/main/java/kr/ac/hansung/Fibonacci.java
+++ src/main/java/kr/ac/hansung/Fibonacci.java
@@ -16,7 +16,7 @@ public class Fibonacci {
         for (int i = 0; i < n; i++) {
             //System.out.print(fibonacci(i) + " ");
             int fib = fibonacci(i);
-            System.out.print("fibonacci(" + i + ")="+ fib);
+            System.out.println("fibonacci(" + i + ")="+ fib);
             System.out.println();
         }
     }
